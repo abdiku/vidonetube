1bc7b586dc7fb280664d3d319cd05925d5c4ec3b
abdiku <rahomabdi11@gmail.com>
2025-07-16 07:41:38 +0700

    Update sumban.js

--- sumban.js
+++ sumban.js
@@ -3,6 +3,6 @@
         <b:if cond='data:blog.pageType == &quot;item&quot;'>
   <div style='text-align:center;margin-top:30px;padding:20px;border-top:1px solid #ccc;'>
     <h3>🙏 Support Us</h3>
-    <a href='/p/donasi.html' style='background:#4CAF50;color:#fff;padding:10px 20px;border-radius:5px;text-decoration:none;' target='_blank'>💚 Donate Now</a>
+    <a href='https://vidonetube.blogspot.com/p/donate.html' style='background:#4CAF50;color:#fff;padding:10px 20px;border-radius:5px;text-decoration:none;' target='_blank'>💚 Donate Now</a>
   </div>
 </b:if>  
